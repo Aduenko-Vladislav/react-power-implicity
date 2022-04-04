a83e953ee26a9e24486872f7ae34f5e17c786c2f
Vladislav Aduenko <adyenko@gmail.com>
2022-04-04 23:50:38 +0300

    feat done landing page

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,27 @@
+import "./styles.scss";
+
+const Header = () => {
+  return (
+    <header className="content">
+      <div className="content__wrapper">
+        <h1 className="content__title">The Power of Simplicity</h1>
+        <p className="content__text">
+          Instead of spending time searching for the right app, our AI will
+          bring the right app to you.
+        </p>
+        <button className="content__btn" type="button">
+          Learn
+        </button>
+        <ol className="slider">
+          <li className="slider__indicator"></li>
+          <li className="slider__indicator"></li>
+          <li className="slider__indicator slider__indicator--active"></li>
+          <li className="slider__indicator"></li>
+          <li className="slider__indicator"></li>
+        </ol>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Main/Main.tsx
@@ -0,0 +1,14 @@
+import "./styles.scss";
+import Nav from "../Nav/Nav";
+import Header from "../Header/Header";
+
+const Main = () => {
+  return (
+    <div className="main">
+      <Nav />
+      <Header />
+    </div>
+  );
+};
+
+export default Main;
